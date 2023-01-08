2b15408c060531f67b29b00b99c6b1ab61bbd9dc
Sagar Parkhi <119633601+parkhisagar@users.noreply.github.com>
2023-01-07 19:34:58 -0500

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title('My Parents New Healthy Diner')
 st.subheader('Breakfast Menu')
-st.write('omega 3 & Blueberry oatmeal')
+st.write('Omega 3 & Blueberry oatmeal')
 st.write('Kale, spinach & rocket smoothie')
 st.write('Hard-boiled Free-Range egg')
 
