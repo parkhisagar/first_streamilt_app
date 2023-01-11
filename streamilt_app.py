b5b774fb34f8c1a7a72374306c842e6c4f77879c
Sagar Parkhi <119633601+parkhisagar@users.noreply.github.com>
2023-01-11 18:03:05 -0500

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -42,7 +42,8 @@ st.header("The fruit load list contains:")
 st.dataframe(my_data_rows)
 
 add_my_fruit = st.text_input('What fruit would you like to add?','Jackfruit')
-#st.dataframe(add_my_fruit)
+
+st.dataframe(add_my_fruit)
 
 st.write('Thanks for adding - ', add_my_fruit)
 
