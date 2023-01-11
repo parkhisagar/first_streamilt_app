e1fa13bf54a92cdb940d0db3dd734c89769b6ba4
Sagar Parkhi <119633601+parkhisagar@users.noreply.github.com>
2023-01-11 18:40:34 -0500

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -25,17 +25,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
-try:
-    fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-    if not fruit_choice:
-        st.error("Please select a fruit to get information")
-    else:    
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-        # put it in dataframe
-        st.dataframe(fruityvice_normalized)
- except URLError as e:
-        st.error()
+    try:
+        fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+        if not fruit_choice:
+            st.error("Please select a fruit to get information")
+        else:    
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+            fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+            # put it in dataframe
+            st.dataframe(fruityvice_normalized)
+     except URLError as e:
+            st.error()
 
 st.stop()
 
