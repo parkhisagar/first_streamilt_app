e21ba1190dab85bb68841770ed4e518d7c0267ac
Sagar Parkhi <119633601+parkhisagar@users.noreply.github.com>
2023-01-10 21:36:01 -0500

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -23,8 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 
 # bring in table with row column form with normalize
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
