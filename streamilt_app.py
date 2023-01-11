57ca8975da555a7c3accb771ad93c1a745ef636c
Sagar Parkhi <119633601+parkhisagar@users.noreply.github.com>
2023-01-10 21:37:39 -0500

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -23,6 +23,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 
 # bring in table with row column form with normalize
