3fd116bad105621d60724c3066724ec43e67e237
Sagar Parkhi <119633601+parkhisagar@users.noreply.github.com>
2023-01-11 18:29:51 -0500

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import snowflake.connector as sc
-from urllib.error import urlerror
+from urllib.error import URLerror
 
 st.title('My Parents New Healthy Diner')
 st.subheader('Breakfast Favorites')
