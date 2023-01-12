b7693fc987d61343fb833da522dd0acfd2f1f719
Sagar Parkhi <119633601+parkhisagar@users.noreply.github.com>
2023-01-11 22:03:32 -0500

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -53,7 +53,7 @@ if st.button('Get Fruit Load List'):
    my_data_rows = get_fruit_load_list()
    st.dataframe(my_data_rows)
    
-st.stop()
+#st.stop()
 
 def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
