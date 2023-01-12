1469af01805ebc47425d85d2ee6c9a008a843e50
Sagar Parkhi <119633601+parkhisagar@users.noreply.github.com>
2023-01-11 21:38:53 -0500

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -36,7 +36,7 @@ try:
      st.error("Please select a fruit to get information")
   else:    
      back_from_function = get_fruityvice_data(fruit_choice)
-     st.dataframe(back_from_funcation)
+     st.dataframe(back_from_function)
 except URLError as e:
     st.error()
 
